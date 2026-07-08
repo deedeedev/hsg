69006b197de65368144f5e1b34afacb28580d604
Davide <davide.sintini@gmail.com>
2026-07-08 17:01:09 +0200

    fix(homebrew): update SHA256 for v1.0.0 tag at 7a73aaa

--- contrib/homebrew/hsg.rb
+++ contrib/homebrew/hsg.rb
@@ -4,7 +4,7 @@ class Hsg < Formula
   desc "Chinese text coverage analysis and comprehensible sentence mining"
   homepage "https://github.com/deedeedev/hsg"
   url "https://github.com/deedeedev/hsg/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "826fab34bc560a43850666e9da10fd1ed55d2aaabbf9c9c9c803a4cd06a60751"
+  sha256 "8d0c4fb08b8b70369ddab931716343db0f30d631e291cf2292fe76918da90274"
   license "Apache-2.0"
 
   depends_on "python@3.12"
